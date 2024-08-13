b8383c39af7d3f9f1c82664ccd017f48da8e456e
Abhishekkumar-7050 <abhishekofficial7050>
2024-08-13 10:16:03 +0530

    handled log in and signup  with google and email password with firebase

---
+++ src/app/login/page.js
@@ -0,0 +1,18 @@
+// pages/auth.js
+
+import Login from "../components/login";
+
+
+
+
+const Auth = () => {
+  return (
+    <div>
+      <h1>Authentication</h1>
+      
+      <Login/>
+    </div>
+  );
+};
+
+export default Auth;
---
+++ src/app/signup/page.js
@@ -0,0 +1,14 @@
+// pages/auth.js
+
+import Signup from "../components/signup";
+const Auth = () => {
+  return (
+    <div>
+      <h1>Authentication</h1>
+      <Signup/>
+      
+    </div>
+  );
+};
+
+export default Auth;
